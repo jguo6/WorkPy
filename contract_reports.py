e9c283c2f3ae25a60467967098c8a63ee982c708
jguo6 <jguo@peak6.com>
2017-01-27 09:15:58 -0600

    Rolling 30s window, counts for contracts, $deltas

--- contract_reports.py
+++ contract_reports.py
@@ -42,18 +42,20 @@ engine = create_engine('postgresql://execution:execution@' + host + ':' + str(po
 
 
 def fills():
+    #filled orders, simple count given each 30 second rolling window 
     qry = '''
     with fills as (select root_id, LEFT(CAST(execution_time as text), 19) as execution_time, ROW_NUMBER() OVER (PARTITION BY root_id order by create_time asc) as rnk
     from events.fills_201611 
-    where order_state = 'FILLED' and security like 'O:%%')
+    where security like 'O:%%')
     select execution_time, root_id
     from fills f
     where f.rnk = 1
-    '''
+    ''' 
+    #
     frame = psql.read_sql(qry, engine)[['root_id', 'execution_time']]
     frame.rename(columns = {'execution_time': 'time',
                             'root_id': 'root'}, inplace=True)
-    frame.reset_index(drop=True, inplace=True)
+    frame.reset_index(drop=True, inplace=True) #
     frame['time'] = frame['time'].apply(lambda x: '2016-11-01 ' + x[11:19])
     frame['time'] = pd.to_datetime(frame['time'])
     frame['newTime'] = frame['time'].apply(lambda dt: datetime.datetime(dt.year, dt.month, dt.day, dt.hour, dt.minute, 30 * (dt.second // 30)))
@@ -64,8 +66,10 @@ def fills():
             day_times[stamp] += 1
         else:
             day_times[stamp] = 1
+    print frame
     return day_times
 
+    
 def openOrders():
     qry = '''
     with something as (select root_id, LEFT(CAST(create_time as text), 19) as create_time, ROW_NUMBER() OVER(PARTITION BY root_id order by execution_time asc) as rk  
@@ -94,6 +98,7 @@ def openOrders():
 
 
 def cancels():
+    #cancels based on contract per count for each 30 second window 
     qry = '''
     with cancels as (select root_id, LEFT(CAST(create_time as text), 19) as create_time, ROW_NUMBER() OVER (PARTITION BY root_id order by create_time asc) as rnk
     from events.xi_201611
@@ -119,11 +124,72 @@ def cancels():
             day_times[stamp] = 1
     return day_times
 
-
-
     
+def dollar_delta_fills():
+    qry = '''
+    with orders as (select root_id, LEFT(CAST(execution_time as text), 19) as execution_time, order_id, fill_price, fill_quantity, security, side, stock_last, order_state, order_state_filled_quantity, order_state_order_price
+    from events.fills_201611
+    where security like 'O:%%'), 
+    deltas as (select root_id, delta, greek_spot_price
+    from events.greeks_201611
+    where security_id like 'O:%%' and from_pid = 0)
+    select x.root_id, x.execution_time, (fill_quantity * delta * stock_last) as DollarDelta, x.order_id, x.fill_price, x.fill_quantity, x.security, x.side, x.stock_last, x.order_state_filled_quantity, x.order_state_order_price, y.delta, y.greek_spot_price
+    from orders x
+    left join deltas y on x.root_id = y.root_id
+    '''
+    frame = psql.read_sql(qry, engine)[['root_id', 'execution_time', 'dollardelta', 'order_id', 'fill_price', 'fill_quantity', \
+         'security', 'side', 'stock_last', 'order_state_filled_quantity', 'order_state_order_price']]
+    frame.rename(columns = {'root_id': 'root_id',
+                            'execution_time': 'time',
+                            'dollardelta': 'dollardelta',
+                            'order_id': 'order_id',
+                            'fill_price': 'fill_price',
+                            'fill_quantity': 'fill_quantity',
+                            'security': 'security',
+                            'side': 'side',
+                            'stock_last': 'stock_last',
+                            'order_state_filled_quantity': 'order_state_filled_quantity',
+                            'order_state_order_price': 'order_state_order_price'}, inplace=True)
+    frame.reset_index(drop=True, inplace=True)
+    frame['time'] = frame['time'].apply(lambda x: '2016-11-01 ' + x[11:19])
+    frame['time'] = pd.to_datetime(frame['time'])
+    frame['newTime'] = frame['time'].apply(lambda dt: datetime.datetime(dt.year, dt.month, dt.day, dt.hour, dt.minute, 30 * (dt.second // 30)))
+    frame['absddelta'] = abs(frame['dollardelta'])
 
-def calculations(fill, cancel):
+    day_times = {}
+    frame['newTime'] = frame['newTime'].apply(lambda dt: '%s:%s:%s' % (dt.hour, dt.minute, dt.second))
+    totaldeltas = frame['dollardelta'].values.sum() 
+    for x in range(len(frame['newTime'])):
+        if frame['newTime'][x] in day_times:
+            day_times[frame['newTime'][x]] += frame['dollardelta'][x] / totaldeltas 
+        else:
+            day_times[frame['newTime'][x]] = frame['dollardelta'][x] / totaldeltas 
+    print day_times
+
+    return frame
+
+
+def dollar_delta_breakdown(frame):
+    totalDeltas = frame['dollardelta'].values
+    totalDeltas = totalDeltas.sum()
+    
+    #dayView = frame.groupby(['rDay']).sum()
+    #gets dollar deltas as a breakdown of the total, maybe should also group it by day and get that percentage as well? 
+    window = {} #How to do it 
+    for x in range(len(frame['dollardelta'])):
+        if frame['newTime'][x] in window:
+            print 'this is the dollar delta'
+            print frame['dollardelta'][x]
+            window[frame['newTime'][x]] += frame['dollardelta'][x] / totalDeltas
+        else:
+            print 'we are inserting something new into the dictionary'
+            print frame['dollardelta'][x]
+            window[frame['newTime'][x]] = frame['dollardelta'][x] / totalDeltas
+    print window 
+    return window
+    #
+    
+def calculations(fill, cancel):    
     fill_cancel = {}
     fill_total = {}
     for x in fill:
@@ -132,18 +198,15 @@ def calculations(fill, cancel):
             fill_total[x] = fill[x] / (cancel[x] + fill[x])
         else:
             fill_total[x] = 1
-
-    print fill_cancel
+    print fill_cancel 
     return fill_cancel 
     
-    
-def dollar_delta():
-    #
-    return 
-    
+    #stop running around 15 minutes, 
     
 if __name__ == "__main__" :
 #    f = fills()
 #    c = cancels()
 #    y = calculations(f, c)
-    print liquidity()
+    dollar_delta_frame = dollar_delta_fills()
+    dollar_delta_breakdown(dollar_delta_frame)
+    #frame breakdown and then convert it to dataframe format
